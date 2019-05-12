220cb9610a32d040053c782726ea5b583cb36c9a
YoungVader <youngvader93@gmail.com>
2019-05-12 19:52:43 +0300

    trying my first spring Web Application

---
+++ src/main/java/ru/Chausov/toDoList/toDoList/IndexController.java
@@ -0,0 +1,20 @@
+package ru.Chausov.toDoList.toDoList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public ModelAndView index() {
+        Map<String, String> model = new HashMap<>();
+
+        model.put("name", "Aleksandr");
+
+        return new ModelAndView("index", model);
+    }
+}
