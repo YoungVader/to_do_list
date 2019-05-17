75a80eeabeee1d4cdf2aa3a323183b6628fe2f60
YoungVader <youngvader93@gmail.com>
2019-05-17 14:19:09 +0300

    added lombock

--- src/main/java/ru/chausov/to_do_list/data_base/entities/Visit.java
+++ src/main/java/ru/chausov/to_do_list/data_base/entities/Visit.java
@@ -1,9 +1,16 @@
 package ru.chausov.to_do_list.data_base.entities;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Builder
+@Getter
+@Setter
 @Entity
 public class Visit {
     @Id
@@ -11,12 +18,4 @@ public class Visit {
     public Long id;
 
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
