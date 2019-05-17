74b0ad4ea037e3c3c9f0d6e94dd474a6df18d4f1
YoungVader <youngvader93@gmail.com>
2019-05-17 14:46:00 +0300

    added lombock

--- src/main/java/ru/chausov/to_do_list/data_base/repositories/VisitsRepository.java
+++ src/main/java/ru/chausov/to_do_list/data_base/repositories/VisitsRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.chausov.to_do_list.data_base.entities.Visit;
 
-@Repository
 public interface VisitsRepository extends CrudRepository<Visit, Long> {
 }
