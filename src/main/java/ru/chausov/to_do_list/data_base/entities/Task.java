878abfec67e1e3808d4b26419c0552f09597b36c
YoungVader <youngvader93@gmail.com>
2019-05-24 21:31:05 +0300

    added default and all args constructors (needed to use in CrudRepository)

--- src/main/java/ru/chausov/to_do_list/data_base/entities/Task.java
+++ src/main/java/ru/chausov/to_do_list/data_base/entities/Task.java
@@ -1,7 +1,9 @@
 package ru.chausov.to_do_list.data_base.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,13 +13,15 @@ import java.util.Date;
 @Data
 @Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
     @Id
     @GeneratedValue
-    final private Long id;
+    private Long id; //how to make final to get through might be uninitialised?
     private String name;
     private String description;
-    final private Long userId;
+    private Long userId; //how to make final to get through might be uninitialised?
     private Date receivedDate;
     private Date toBeDone;
     private boolean done;
--- src/main/java/ru/chausov/to_do_list/data_base/entities/User.java
+++ src/main/java/ru/chausov/to_do_list/data_base/entities/User.java
@@ -1,7 +1,9 @@
 package ru.chausov.to_do_list.data_base.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,10 +13,12 @@ import java.util.Date;
 @Data
 @Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue
-    final private Long id;
+    private Long id; //how to make final to get through might be uninitialised?
     private String name;
     private String lastName;
     private Date birthDate;
