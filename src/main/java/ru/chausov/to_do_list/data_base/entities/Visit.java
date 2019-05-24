1404ddaf8e447411ecd0be8921516dcd93fa60b2
YoungVader <youngvader93@gmail.com>
2019-05-24 21:29:06 +0300

    added default and all args constructors (needed to use in CrudRepository)

--- src/main/java/ru/chausov/to_do_list/data_base/entities/Visit.java
+++ src/main/java/ru/chausov/to_do_list/data_base/entities/Visit.java
@@ -1,7 +1,6 @@
 package ru.chausov.to_do_list.data_base.entities;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,10 +10,12 @@ import javax.persistence.Id;
 @Data
 @Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Visit {
     @Id
     @GeneratedValue
-    final private Long id;
+    private Long id; //how to make final to get through might be uninitialised?
 
     private String description;
 }
