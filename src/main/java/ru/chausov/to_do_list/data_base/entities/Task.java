dfa094147545ec7b822efc595f933347f1ea375c
YoungVader <youngvader93@gmail.com>
2019-05-26 18:56:03 +0300

    added @Table(name="tasks")

--- src/main/java/ru/chausov/to_do_list/data_base/entities/Task.java
+++ src/main/java/ru/chausov/to_do_list/data_base/entities/Task.java
@@ -8,12 +8,14 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 
 @Data
 @Builder
 @Entity
+@Table(name="tasks")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Task {
