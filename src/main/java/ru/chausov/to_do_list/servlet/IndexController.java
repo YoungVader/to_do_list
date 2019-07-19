6391a695a6a983da8f3de23faa6825a2d41ba7c0
YoungVader <youngvader93@gmail.com>
2019-07-19 16:17:10 +0300

    removed unused imports

--- src/main/java/ru/chausov/to_do_list/servlet/IndexController.java
+++ src/main/java/ru/chausov/to_do_list/servlet/IndexController.java
@@ -1,8 +1,6 @@
 package ru.chausov.to_do_list.servlet;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
