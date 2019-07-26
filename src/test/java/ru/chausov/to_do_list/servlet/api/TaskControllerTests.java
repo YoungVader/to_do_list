715662563a669a2b401caf0fe57e21b3ce760cbc
YoungVader <youngvader93@gmail.com>
2019-07-26 11:32:49 +0300

    addTaskTest added

--- src/test/java/ru/chausov/to_do_list/servlet/api/TaskControllerTests.java
+++ src/test/java/ru/chausov/to_do_list/servlet/api/TaskControllerTests.java
@@ -4,13 +4,24 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.ModelAndView;
 import ru.chausov.to_do_list.data_base.entity.Task;
+import ru.chausov.to_do_list.data_base.entity.User;
 import ru.chausov.to_do_list.data_base.repository.TaskRepository;
+import ru.chausov.to_do_list.data_base.repository.UserRepository;
 import ru.chausov.to_do_list.servlet.TaskController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,17 +33,33 @@ public class TaskControllerTests {
     private TaskController taskController;
     @Autowired
     private TaskRepository taskRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-//    @Test
-//    public void addTaskTest() {
-//        Task taskToAdd = new Task();
-//
-//        taskToAdd.setDescription("TestDescription");
-//        Task addedTask = taskController.addTask(taskToAdd);
-//
-//        Assert.assertEquals(taskToAdd, addedTask);
-//        Assert.assertEquals(taskToAdd.getDescription(), addedTask.getDescription());
-//    }
+
+    @WithMockUser(username = "user", password = "123")
+    @Test
+    public void addTaskTest() {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = User.builder().username("user").password("123").build();
+
+        userRepository.save(user);
+
+        Task taskToAdd = Task.builder().user(user).build();
+
+        taskToAdd.setDescription("TestDescription");
+
+        Map<String, Object> model = new HashMap<>();
+
+        ModelAndView modelAndView = taskController.addTask(auth, taskToAdd, model);
+
+        Task addedTask = (Task) modelAndView.getModel().get("task");
+
+        Assert.assertEquals(taskToAdd, addedTask);
+        Assert.assertEquals(taskToAdd.getDescription(), addedTask.getDescription());
+    }
 
     @Test
     public void updateTaskTest() {
