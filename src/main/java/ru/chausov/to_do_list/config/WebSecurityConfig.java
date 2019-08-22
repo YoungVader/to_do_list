265555b6a6e5a9245e04fd128e32f0d8f1cb5391
YoungVader <50447546+YoungVader@users.noreply.github.com>
2019-08-22 18:49:05 +0300

    added BCrypt password encoder

--- src/main/java/ru/chausov/to_do_list/config/WebSecurityConfig.java
+++ src/main/java/ru/chausov/to_do_list/config/WebSecurityConfig.java
@@ -2,11 +2,13 @@ package ru.chausov.to_do_list.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ru.chausov.to_do_list.data_base.entity.User;
 import ru.chausov.to_do_list.data_base.repository.UserRepository;
@@ -18,9 +20,16 @@ import javax.sql.DataSource;
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
-    private DataSource dataSource;
+    private PasswordEncoder passwordEncoder;
+    private final DataSource dataSource;
     private final UserRepository userRepository;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -44,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         if(userRepository.findByUsername("admin") == null) {
             User user = User.builder().username("admin")
-                    .password("123")
+                    .password(passwordEncoder.encode("123"))
                     .name("")
                     .lastName("")
                     .birthDate("")
@@ -59,7 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .passwordEncoder(passwordEncoder)
                 .usersByUsernameQuery("select username, password, active from users where username=?")
                 .authoritiesByUsernameQuery("select username, role from users where username=?");
     }
--- src/main/java/ru/chausov/to_do_list/servlet/UserController.java
+++ src/main/java/ru/chausov/to_do_list/servlet/UserController.java
@@ -3,6 +3,8 @@ package ru.chausov.to_do_list.servlet;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +28,8 @@ import java.util.Map;
 public class UserController {
 
     private final UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Transactional
     @GetMapping("/table")
@@ -88,6 +92,7 @@ public class UserController {
 
         user.setRole(Role.USER);
 
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         model.put("user", userRepository.save(user));
 
         return new ModelAndView("redirect:/login", model);
