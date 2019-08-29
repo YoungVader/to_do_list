bf16a2707f6a2e8803deec3bd219d9a2cb861467
YoungVader <50447546+YoungVader@users.noreply.github.com>
2019-08-29 19:52:47 +0300

    Added username in greetings for index page

--- src/main/java/ru/chausov/to_do_list/servlet/IndexController.java
+++ src/main/java/ru/chausov/to_do_list/servlet/IndexController.java
@@ -3,10 +3,13 @@ package ru.chausov.to_do_list.servlet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 import ru.chausov.to_do_list.data_base.entity.Visit;
 import ru.chausov.to_do_list.data_base.repository.VisitRepository;
 
+import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 
 @Controller
@@ -26,8 +29,10 @@ public class IndexController {
     }
 
     @GetMapping("/index")
-    public String index() {
-        return "index";
+    public ModelAndView index(Principal authUser, Map<String, Object> model) {
+        model.put("username", authUser.getName());
+
+        return new ModelAndView("index", model);
     }
 
     @GetMapping("/login")
